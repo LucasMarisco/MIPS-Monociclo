9e29a71c1f450f90a76dc3531641ae3295c1d01a
Lucas Marisco <lucasmarisco27@gmail.com>
2024-11-22 20:00:41 -0300

    Ctrl-ULA montado

--- MIPS-Monociclo/Ctrl-ULA.vhd
+++ MIPS-Monociclo/Ctrl-ULA.vhd
@@ -3,17 +3,35 @@ USE ieee.std_logic_1164.ALL;
 
 ENTITY Ctrl-ULA IS
     PORT (
-        A,B: in STD_LOGIC_VECTOR(31 DOWNTO 0); -- Entradas
-		  Controle: in STD_LOGIC_VECTOR(2 DOWNTO 0); -- Sequencia de bits para 
-		  Zero: out STD_LOGIC; -- Nao entendi como que funciona
-		  Resultado: out STD_LOGIC_VECTOR(31 DOWNTO 0)
+        ULAOp: in STD_LOGIC_VECTOR(1 DOWNTO 0); -- Sequencia de bits para 
+        funct: in STD_LOGIC_VECTOR(5 DOWNTO 0); -- Entradas
+		cULA: out STD_LOGIC_VECTOR(2 DOWNTO 0); -- Nao entendi como que funciona
     );
 END ENTITY;
 
 ARCHITECTURE arch OF Ctrl-ULA IS
-	-- Signals
-	SIGNAL Resultado_Soma_Subtracao, Resultado_AND_OR: STD_LOGIC_VECTOR(31 DOWNTO 0);
-	SIGNAL CarryOut_Soma: STD_LOGIC;
+
+cULA <= "010" when ULAOp = "00" else 
+        "110" when ULAOp = "01" else
+        "010" when funct = "100000" else -- add
+        "110" when funct = "100010" else -- sub
+        "000" when funct = "100100" else -- and
+        "001" when funct = "100101" else -- or
+        "111" when funct = "101010" else -- slt (set on less then)
+        "010"; -- coloquei para ele ser uma somador se o OP code for "11" ou se o funct for algo não definido    
+
+
+
+--valores tirando da tabela dos Slide 8T.56
+--tá um pouco diferente dos que temos de referência, talvez mude alguma coisa no bloco de comando por que
+--temos que deixar a ULAOp em 10, no deles parece que não precisa, mas tem que ver como eles fizeram
+--podemos fazer usando:      if-then-else               case                when-else
+--                          mau otimizado           1 variavel só           perigoso(pq depende da ordem)
+--                        mais claro e garantido                            otimizado
+
+-- no nosso caso, se ele passar do 2 primeiros quer dizer que ULAOp é ou "10" ou "11", sendo assim
+-- podemos começar a analizar o funct, por isso fomos de when else
+
 BEGIN
 		
 END ARCHITECTURE arch;
